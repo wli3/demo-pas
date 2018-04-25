61bf30ce217ce97a5ae1b95d3b8d4d9096c665c9
William Li <wul@microsoft.com>
2018-04-24 22:21:08 -0700

    Spell

--- lib/hacker_news_notifier/title_checker.rb
+++ lib/hacker_news_notifier/title_checker.rb
@@ -11,9 +11,9 @@ module HackerNewsNotifier
 
       url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
       response = RestClient.get(url)
-      paresed = JSON.parse(response).first top
+      parsed = JSON.parse(response).first top
 
-      paresed.each do |item_id|
+      parsed.each do |item_id|
         item_url = "https://hacker-news.firebaseio.com/v0/item/#{item_id}.json"
         item_response = RestClient.get(item_url)
         return true if JSON.parse(item_response)['title'].include? title
